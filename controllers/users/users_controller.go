9e4731a25eed27fe639b375cd82772d74d13866f
iantuan <ian.tuan@gmail.com>
2022-05-13 00:38:37 +0800

    complete create user

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,13 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/iantuan/bookstore_users-api/domain/users"
+	"github.com/iantuan/bookstore_users-api/services"
+	"github.com/iantuan/bookstore_users-api/utils/errors"
 )
 
 var (
@@ -11,7 +15,37 @@ var (
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement CreateUser!")
+
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		fmt.Println(err)
+		return
+	}
+
+	//fmt.Println(user)
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	//TODO: Handle error
+	//	return
+	//}
+	//if err := json.Unmarshal(bytes, &user); err != nil {
+	//	fmt.Println("json unmarshal error")
+	//	fmt.Println(err.Error())
+	//	//fmt.Println(string(bytes))
+	//	//TODO: Handle json error
+	//	return
+	//}
+
+	result, saveErr := services.CreateUser(user)
+
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	//fmt.Println(result)
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
